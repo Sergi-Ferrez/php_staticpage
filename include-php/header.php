eeedf2eed1beab2ba8f5f989fcfb96c7ae66b943
Sergi-Ferrez <sergiferrezpuig@gmail.com>
2020-10-24 21:05:51 +0200

    Commit

---
+++ include-php/header.php
@@ -0,0 +1,14 @@
+<div class="row">
+    <div class="col-6 flx_center">
+        <h1 id="webTitle">Amazing</h1>
+    </div>
+    <div class="col-6 flx_center">
+        <ul id="menu">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Inicio</a></li>
+        </ul>
+    </div>
+
+</div>
